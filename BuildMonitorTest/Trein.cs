3c51d824c0810991416922cb2f7aa82591f25760
abackwood <a.backwood@gmail.com>
2016-09-05 09:45:42 +0200

    Twee projecten aangemaakt: hoofdproject en bijbehorende Unit TEst project

---
+++ BuildMonitorTest/Trein.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildMonitorTest {
+    public class Trein {
+        public int Serienummer { get; private set; }
+        public int Ritnummer { get; private set; }
+        public int AantalPassagiers { get; private set; }
+
+        public Trein(int serienummer, int ritnummer) {
+            this.Serienummer = serienummer;
+            this.Ritnummer = ritnummer;
+            this.AantalPassagiers = 0;
+        }
+
+        public void VeranderAantalPassagiers(int delta) {
+            if (AantalPassagiers + delta < 0) {
+                throw new ArgumentException("Meer passagiers verwijderd dan aantal in de trein");
+            }
+
+            AantalPassagiers += delta;
+        }
+
+        public Trein genereerVolgendeTrein() {
+            return new Trein(Serienummer, Ritnummer + 1);
+        }
+    }
+}
---
+++ BuiltMonitorTestTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BuildMonitorTest;
+
+namespace BuildMonitorTestTest {
+    [TestClass]
+    public class TreinTest {
+
+        [TestMethod]
+        public void AantalPassagiersWordtCorrectVerhoogd() {
+            Trein trein = new Trein(1,1);
+            int oudAantal = trein.AantalPassagiers;
+            int verandering = 5;
+            int verwachtAantal = oudAantal + verandering;
+
+            trein.VeranderAantalPassagiers(verandering);
+
+            Assert.AreEqual(verwachtAantal,trein.AantalPassagiers);
+        }
+
+        [TestMethod]
+        [ExpectedException (typeof (ArgumentException))]
+        public void AantalPassagiersKanNietNegatiefZijn() {
+            Trein trein = new Trein(1,1);
+            trein.VeranderAantalPassagiers(5);
+
+            trein.VeranderAantalPassagiers(-10);
+        }
+    }
+}
