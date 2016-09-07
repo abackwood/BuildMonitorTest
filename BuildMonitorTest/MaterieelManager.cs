b45983831ddbf349a2ac95fd96a2a3183cc24d85
abackwood <a.backwood@gmail.com>
2016-09-07 13:53:24 +0200

    Revert "Revert "MaterieelManager toegevoegd die prototypes voor Materieel registreert""
    This reverts commit fd33975bcec724ee4885d5567ddff9aa17f4d462.

---
+++ BuildMonitorTest/MaterieelManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildMonitorTest {
+    public class MaterieelManager {
+        private Dictionary<string,Materieel> prototypes;
+
+        public MaterieelManager() {
+            prototypes = new Dictionary<string,Materieel>();
+            VulPrototypesIn();
+        }
+
+        public Materieel MaakMaterieelVanType(string type) {
+            return new Materieel(prototypes[type]);
+        }
+
+        private void VulPrototypesIn() {
+            prototypes["sprinter"] = new Materieel(100,60);
+            prototypes["intercity"] = new Materieel(200,120);
+        }
+    }
+}
