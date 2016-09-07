bfb3c5b95025a09feb4bdcd261480824a03236d4
abackwood <a.backwood@gmail.com>
2016-09-07 13:40:26 +0200

    Materieel heeft nu maximum snelheid

--- BuildMonitorTest/Materieel.cs
+++ BuildMonitorTest/Materieel.cs
@@ -7,9 +7,11 @@ using System.Threading.Tasks;
 namespace BuildMonitorTest {
     public class Materieel {
         public int PassagiersCapaciteit { get; private set; }
+        public double MaximumSnelheid { get; private set; }
 
-        public Materieel(int capaciteit) {
+        public Materieel(int capaciteit, double maximumSnelheid) {
             this.PassagiersCapaciteit = capaciteit;
+            this.MaximumSnelheid = maximumSnelheid;
         }
     }
 }
