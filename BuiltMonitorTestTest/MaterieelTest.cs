349048426d61a4195476ba88b981697c3106cb72
abackwood <a.backwood@gmail.com>
2016-09-09 15:00:59 +0200

    merged wierndess

---
+++ BuiltMonitorTestTest/MaterieelTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BuildMonitorTest;
+
+namespace BuildMonitorTestTest {
+    [TestFixture]
+    public class MaterieelTest {
+        [Test]
+        public void InstantieHeeftZelfdeEigenschappenAlsPrototype() {
+            Materieel proto = new Materieel(100,100);
+
+            Materieel instantie = new Materieel(proto);
+
+            Assert.AreEqual(proto.PassagiersCapaciteit,instantie.PassagiersCapaciteit);
+            Assert.AreEqual(proto.MaximumSnelheid,instantie.MaximumSnelheid);
+        }
+    }
+}
